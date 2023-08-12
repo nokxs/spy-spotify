1182b430d7d3ea01506a12a05da5ee494c9acaff
Niklas <niklas@nokxs.de>
2023-08-12 21:42:09 +0200

    Enable advanced_file_group_media_in_folders_enabled by default

--- EspionSpotify/Properties/Settings.Designer.cs
+++ EspionSpotify/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace EspionSpotify.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool advanced_file_group_media_in_folders_enabled {
             get {
                 return ((bool)(this["advanced_file_group_media_in_folders_enabled"]));
